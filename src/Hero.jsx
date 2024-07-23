2cd61db57c21b95ecb807ee6ea03c7b35b48fae0
anjakely18 <anjaniaina03@gmail.com>
2024-07-23 10:42:41 +0200

    modification hero

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -6,12 +6,17 @@ const Hero = () => {
     <section className="hero">
       <div className="hero-center">
         <div className="hero-title">
-          <h1>Contentful CMS</h1>
+          <h1>Mes Projets React</h1>
           <p>
-            Pitchfork schlitz tonx, coloring book celiac tousled succulents
-            ascot affogato cardigan jianbing crucifix seitan. Synth man braid
-            everyday carry try-hard pour-over keffiyeh slow-carb sriracha
-            chillwave banjo gochujang kinfolk small batch mustache
+            Bienvenue sur mon site web dédié à la présentation de mes mini
+            projets en React ! Ici, vous découvrirez une collection variée de
+            projets que j'ai réalisés pour explorer et maîtriser les différentes
+            facettes de cette bibliothèque JavaScript puissante. Chaque projet
+            met en avant des fonctionnalités spécifiques, des techniques
+            modernes et des solutions créatives que j'ai développées pour
+            répondre à des défis particuliers. J'espère que vous apprécierez
+            cette vitrine de mon travail et que cela vous inspirera dans vos
+            propres aventures en développement web. Bonne exploration !
           </p>
         </div>
         <div className="img-container">
